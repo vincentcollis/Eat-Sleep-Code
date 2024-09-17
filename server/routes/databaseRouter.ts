7972da9a14f6bd00aaece676367860247db4565b
Vincent Collis <vincent.collis@icloud.com>
2024-09-17 10:52:41 -0500

    chore: Update tsconfig for improved Express compatibility
    - Modified the  file to optimize TypeScript settings for better integration with Express.
    - Adjusted compiler options to support necessary types and modules, enhancing type safety and developer experience.
    
    This update ensures that the TypeScript configuration aligns with Express requirements, facilitating smoother development and error handling.

--- server/routes/databaseRouter.ts
+++ server/routes/databaseRouter.ts
@@ -2,7 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 
 const databaseRouter = express.Router();
 
-databaseRouter.get('/', (_req: Request, res: Response) => {
+databaseRouter.get('/all', (_req: Request, res: Response) => {
 	res.status(200).send('get all quesitons endpoint');
 });
 
