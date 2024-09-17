0451d497f8fa68061f4dcadf21bde8ebba7c1952
Vincent Collis <vincent.collis@icloud.com>
2024-09-17 15:47:19 -0500

    removed hard coded password

--- server/db.js
+++ server/db.js
@@ -1,8 +1,7 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 // // Establish Connection Pool
-// const SERVER_PASSWORD = process.env.SERVER_PASSWORD;
-const SERVER_PASSWORD = 'zbm4HPE@czj9kvf0mgj';
+const SERVER_PASSWORD = process.env.SERVER_PASSWORD;
 const postgresURI = `postgresql://postgres.bcgzannfugcqgvubsmsy:${SERVER_PASSWORD}@aws-0-us-east-1.pooler.supabase.com:6543/postgres`;
 // // Create a connect pool
 const pool = new Pool({
